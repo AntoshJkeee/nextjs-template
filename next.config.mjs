f0fe95e21fdca2d90b29d570dc826034c39d209f
antoshjkeee <toha_bart@mail.ru>
2024-03-30 21:49:19 +0300

    install package

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+	// basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+	swcMinify: true,
+	images: {
+		// domains: [process.env.NEXT_PUBLIC_IMAGES],
+	},
+};
+
+export default nextConfig;
