5146da3c5abf4d483f7bd46ee44d7d55a1201dbd
AntoshJkeee <sereda@aicrobotics.ru>
2024-04-15 18:04:23 +0300

    fix: test
    test
    
    METADATA: teest

--- commitizen.js
+++ commitizen.js
@@ -41,11 +41,13 @@ module.exports = {
 	],
 	messages: {
 		type: 'Какие изменения вы вносите?',
+		scope: 'Выберите область изменений',
 		subject: 'Напишите КОРОТКИЙ заголовок, описывающий коммит: ',
 		footer: 'Укажите номер таска из ActiveCollab. Например: Task-29027-130, Task-32997-340',
 		body: 'Полное описание проделанной работы с указанием файла, который изменил',
 		confirmCommit: 'Вас устраивает получившийся коммит?',
 	},
+	scopes: [{ name: 'frontend', value: false }],
 	allowBreakingChanges: false,
 	allowCustomScopes: false,
 	footerPrefix: 'METADATA:',
