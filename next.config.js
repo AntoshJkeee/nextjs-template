66602138c5320f1f6e6f3e2650fd1c7bb1518e5e
AntoshJkeee <sereda@aicrobotics.ru>
2024-04-17 13:17:01 +0300

    feat: config
    config
    
    METADATA: task

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@
 module.exports = {
 	// basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 	swcMinify: true,
+	sassOptions: {
+		logger: {
+			sourceMap: true,
+			warn: function (message) {
+				process.stderr.write(`\n${message}\n\n`);
+			},
+			debug: function (message) {
+				process.stderr.write(`\n${message}\n\n`);
+			},
+		},
+	},
 	images: {
 		// domains: [process.env.NEXT_PUBLIC_IMAGES]
 	},
