9a21d5f9e6a7fdb2a7b5e43de7c2453de3c95adc
exocornet <exocornet@gmail.com>
2024-04-18 08:26:04 +0300

    feat: roetes page and create folder pages
    roetes page and create folder pages

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,22 @@
+import './main.scss';
+
+export default function MainPage() {
+	return (
+		<>
+			<main>
+				<h1 className="ttt">Шаблон Next JS 14</h1>
+				{/*<ul className={styles.ul}>*/}
+				{/*	<li className={styles.li}>NEXT JS 14.1.4</li>*/}
+				{/*	<li className={styles.li}>REACT JS 18.2.0</li>*/}
+				{/*	<li className={styles.li}>NEXT JS 14.1.4</li>*/}
+				{/*	<li className={styles.li}>TYPESCRIPT 5.4.3</li>*/}
+				{/*	<li className={styles.li}>PRECOMMIT, LINT-STAGED, COMMIT-LINT, COMMIZEN, GIT-HOOKS</li>*/}
+				{/*	<li className={styles.li}>REACT ICONS (maybe remove package)</li>*/}
+				{/*	<li className={styles.li}>STYLELINT, PRETTIER, ESLINT</li>*/}
+				{/*	<li className={styles.li}>REACT ICONS (maybe remove package)</li>*/}
+				{/*	<li className={styles.li}>SASS, SWISS-KNIFE, POSTCSS</li>*/}
+				{/*</ul>*/}
+			</main>
+		</>
+	);
+}
